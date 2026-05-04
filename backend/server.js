4a8cb0b88958f89c420e9742f13a5ad07d0961ac
Mark Tom <markbironga@gmail.com>
2026-05-04 06:06:48 -0500

    cors

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ const limiter = rateLimit({
 app.use(limiter);
 
 app.use(cors({
-  origin: "https://videoweb.onrender.com/"
+  origin: "https://videoweb.onrender.com"
 }));
 
 app.get("/", handleRoot);
